a5416af9f8f1e019254ca344c0b4107654b8c25a
Ran# <ran-n@tutanota.com>
2024-05-11 14:57:58 +0100

    manually implemented the load and save file functions instead of using the uteis dependency

--- metadatos.py
+++ metadatos.py
@@ -3,7 +3,7 @@
 # -----------------------------------------------------------------------------
 #+ Autor:  	Ran#
 #+ Creado:	18/10/2021 13:50:54
-#+ Editado:	2022/10/30 12:44:23.300656
+#+ Editado:	2024/05/11 14:55:29.743604
 # -----------------------------------------------------------------------------
 
 #* Reescrito do script "metadata" de 2019
@@ -12,9 +12,89 @@
 
 import sys
 from datetime import datetime
+from pathlib import Path
+from typing import Optional, List, Union
 
-from uteis import ficheiro as uf
+# -----------------------------------------------------------------------------
+# UTILS
+# -----------------------------------------------------------------------------
 
+def save_file(
+        fname: str,
+        content: Union[str, List[str]],
+        encoding='utf-8-sig'
+    ) -> bool:
+    """
+    Saves a file to memory once given its name, with or without a path. Without
+    a path its saved to the current folder, otherwise where pointed to.
+
+    @in:
+    ├─ fname
+    │  └ Name of the file, with extension and path.
+    ├─ in_lines
+    │  ├ True: Load in an list each line of the file as a String.
+    │  └ False: Load the whole file as one String.
+    └─ encoding
+       └ Indicates the type of encoding to use.
+
+    @out:
+    ├─ List[String]
+    │  └ Contents of the file in a list divided by line.
+    └─ String
+       └ Contents of the file as one single string.
+    """
+
+    # create the path if non existant
+    """
+    It can be done using the parent attr since the last element of the pathname
+    is the name of the file itself.
+
+    If it has no previous path (like ./file.txt or file.txt) it wont create anything
+    but otherwise the path will be created if needed (like on ./test/test1/file.txt the
+    folder structure created will be ./test/test1 if its non previously existant).
+    """
+    Path(fname).parent.mkdir(parents=True, exist_ok=True)
+
+    with open(fname, 'w', encoding=encoding) as f:
+        f.writelines('%s\n' % line for line in content)
+# -----------------------------------------------------------------------------
+
+def load_file(
+        fname: Union[str, List[str]],
+        in_lines: Optional[bool] =True,
+        encoding: Optional[str] ='utf-8-sig'
+    ) -> Union[str, List[str]]:
+    """
+    Given a filename as a string loads the contents in a list, line by line.
+
+    @in:
+    ├─ fname
+    │  └ Name of the file, with extension and path.
+    ├─ in_lines
+    │  ├ True: Load in an list each line of the file as a String.
+    │  └ False: Load the whole file as one String.
+    └─ encoding
+       └ Indicates the type of encoding to use.
+
+
+    @out:
+    ├─ List[String]
+    │  └ Contents of the file in a list divided by line.
+    └─ String
+       └ Contents of the file as one single string.
+    """
+
+    if not Path(fname).is_file():
+        raise Exception('O ficheiro {} non existe'.format(nome))
+
+    with open(fname, 'r', encoding=encoding) as f:
+        if in_lines:
+            return f.read().splitlines()
+        else:
+            return f.read()
+# -----------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
 # -----------------------------------------------------------------------------
 
 def print_axuda():
@@ -66,7 +146,7 @@ def trocear_prime_linha(prime_linha):
 def mostrar(fich, tipo_fich, prime_linha, vars_esteticas):
     prime_linha, _ = trocear_prime_linha(prime_linha)
     simb_comen = prime_linha.split('!')[0]
-    contido = [ele2 for ele2 in [ele.rstrip() for ele in uf.cargarFich(fich)] if ele2.startswith(simb_comen+vars_esteticas['indicador'], 0)]
+    contido = [ele2 for ele2 in [ele.rstrip() for ele in load_file(fich)] if ele2.startswith(simb_comen+vars_esteticas['indicador'], 0)]
 
     for linha in contido:
         print(linha)
@@ -76,7 +156,7 @@ def mostrar(fich, tipo_fich, prime_linha, vars_esteticas):
 def editar(fich, tipo_fich, prime_linha, vars_esteticas):
     prime_linha, simb_fin_comen = trocear_prime_linha(prime_linha)
     simb_comen = prime_linha.split('!')[0]
-    contido = uf.cargarFich(fich)
+    contido = load_file(fich)
 
     agora = str(datetime.now()).replace('-', vars_esteticas['sep_datas'])
     agora = agora.replace(':', vars_esteticas['sep_horas'])
@@ -89,14 +169,14 @@ def editar(fich, tipo_fich, prime_linha, vars_esteticas):
         if linha.startswith(simb_comen+vars_esteticas['indicador']+' Editado:', 0):
             contido[index] = simb_comen+vars_esteticas['indicador']+' Editado:\t'+agora+simb_fin_comen
 
-    uf.gardarFich(fich, contido)
+    save_file(fich, contido)
 
 # -----------------------------------------------------------------------------
 
 def crear(fich, tipo_fich, prime_linha, vars_esteticas):
     prime_linha, simb_fin_comen = trocear_prime_linha(prime_linha)
     simb_comen  = prime_linha.split('!')[0]
-    contido = uf.cargarFich(fich)
+    contido = load_file(fich)
 
     agora = str(datetime.now()).replace('-', vars_esteticas['sep_datas'])
     agora = agora.replace(':', vars_esteticas['sep_horas'])
@@ -129,21 +209,21 @@ def crear(fich, tipo_fich, prime_linha, vars_esteticas):
             except IndexError:
                 contido.insert(index, insertable)
 
-    uf.gardarFich(fich, contido)
+    save_file(fich, contido)
 
 # -----------------------------------------------------------------------------
 
 def suprimir(fich, tipo_fich, prime_linha, vars_esteticas):
     prime_linha, _ = trocear_prime_linha(prime_linha)
     simb_comen = prime_linha.split('!')[0]
-    contido_ini = uf.cargarFich(fich)
+    contido_ini = load_file(fich)
 
     contido = []
     for linha in contido_ini:
         if not linha.startswith(simb_comen+vars_esteticas['indicador'], 0):
             contido.append(linha)
 
-    uf.gardarFich(fich, contido)
+    save_file(fich, contido)
 
 # -----------------------------------------------------------------------------
 
